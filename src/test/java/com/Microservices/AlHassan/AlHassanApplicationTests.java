3579110e8f8196f508566be6b7f86268e4786bef
Ali Tariq <alibabapk@aol.com>
2023-03-27 02:25:24 +0500

    Initial commit

---
+++ src/test/java/com/Microservices/AlHassan/AlHassanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservices.AlHassan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlHassanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
